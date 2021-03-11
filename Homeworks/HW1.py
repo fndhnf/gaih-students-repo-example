7a66961eac06171454b96da09e23186fd83b5bb0
fndhnf <80281826+fndhnf@users.noreply.github.com>
2021-03-11 12:31:04 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,32 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+#for x in range(a):
+	#print(a)
+	
+#####	
+nums = list(range(100)) #a=100 dersek
+evenlist = []
+oddlist = []
+
+# odds = [i for i in numbers if i%2!=0]  alternatif olabilirdi
+
+for x in nums: 	# bu formatta istendigini dusundugumden, buradan devam ediyorum
+if (x % 2 == 0) : #2ye kalansız bolunuyorsa cıfttir
+	evenlist.append(i)	
+if (x % 2 !== 0) : #else de kullanılabilirdi
+	oddlist.append(i)
+
+print(evenlist)
+print(oddlist)
+
+#####
+mergedlist2 = [] 
+mergedlist = evenlist + oddlist  #listeleri birleştirme aşaması
+mergedlist2 = [i**2 for i in mergedlist] #herbir indexi 2 ile carpma
+print(mergedlist2)
+
+######
+for j in range (0, len(mergedlist2)): 
+	print (j, "th item' data type is:", type(mergedlist2[j]))
+
